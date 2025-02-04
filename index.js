d2845b46ebaa2982ec8f2dbe6b82f5a4aa526531
arunbaaby <arunarunvb0398@gmail.com>
2025-02-04 14:47:34 +0530

    changed DB to atlas

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 //index.js
 const mongoose = require('mongoose');
 //connect mongodb server and also give the name of the database
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_ATLAS_URI);
+
+mongoose.connection.on('error', err => {
+    console.error('MongoDB Connection Error:', err);
+});
+
 // mongoose.connect('mongodb://127.0.0.1:27017/tenue');
 
 // const logger = require('morgan');
